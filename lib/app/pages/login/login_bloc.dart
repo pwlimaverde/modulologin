0a15a9592dceae69ae30bcdb42fb131194c4fb3d
PWLIMAVERDE <vorfast@gmail.com>
2019-10-19 17:03:48 -0300

    login via token funcionando

--- lib/app/pages/login/login_bloc.dart
+++ lib/app/pages/login/login_bloc.dart
@@ -1,7 +1,27 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:modulologin/app/pages/login/model/login_model.dart';
+import 'package:rxdart/rxdart.dart';
 
 class LoginBloc extends BlocBase {
   //dispose will be called automatically by closing its streams
+  final LoginModel model = LoginModel();
+
+  BehaviorSubject<List<LoginModel>> _usersListController = BehaviorSubject.seeded([]);
+  Observable<List<LoginModel>> get usersListOut => _usersListController.stream;
+
+
+  addNovoLogin(LoginModel model){
+    _usersListController.value.add(model);
+    _usersListController.add(_usersListController.value);
+  }
+
+  logout(){
+    _usersListController.add([]);
+    model.token = null;
+    model.username = null;
+    model.password = null;
+  }
+
   @override
   void dispose() {
     super.dispose();
--- lib/app/pages/login/login_repository.dart
+++ lib/app/pages/login/login_repository.dart
@@ -1,5 +1,14 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:dio/dio.dart';
+import 'package:modulologin/app/pages/login/login_bloc.dart';
+import 'package:modulologin/app/pages/login/login_module.dart';
+import 'package:modulologin/app/pages/login/model/login_model.dart';
+import 'package:modulologin/shared/constants.dart';
+
+final loginBloc = LoginModule.to.getBloc<LoginBloc>();
+var model = loginBloc.model;
 
 class LoginRepository extends Disposable {
   Future fetchPost(Dio client) async {
@@ -11,4 +20,35 @@ class LoginRepository extends Disposable {
   //dispose will be called automatically
   @override
   void dispose() {}
+}
+
+class LoginApi {
+  static Future<LoginModel> loginUsers(String username, String password) async {
+
+    var url = BASE_URL;
+    var urlToken = TOKEN_URL;
+    var header = {
+      "Content-type": "application/json",
+    };
+    Map params = {
+      "username": username,
+      "password": password,
+    };
+    var _body = json.encode(params);
+
+    var response = await http.post(url+urlToken, headers: header, body: _body);
+    Map mapResponse = json.decode(response.body);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      model.username = params["username"];
+      model.password = params["password"];
+      model.token = mapResponse["token"];
+      loginBloc.addNovoLogin(model);
+      print(model);
+    } else {
+      model = null;
+    }
+    return model;
+  }
 }
