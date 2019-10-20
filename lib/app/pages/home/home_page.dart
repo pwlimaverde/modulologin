64378fd73e5e92f29f94167ce226bdca3e6afa95
PWLIMAVERDE <vorfast@gmail.com>
2019-10-20 16:41:27 -0300

    automação modulo login ok

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:modulologin/app/app_module.dart';
 import 'package:modulologin/app/shared/mod_login/mod_login_bloc.dart';
 import 'package:modulologin/app/shared/mod_login/model/mod_login_model.dart';
-import 'package:modulologin/app/shared/mod_login/utilitario/rotas_login.dart';
 
 final modLoginBloc = AppModule.to.getBloc<ModLoginBloc>();
 final ModLoginModel model = modLoginBloc.model;
@@ -19,7 +18,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home"),
         actions: <Widget>[
-            getButtonLogin(),
+            modLoginBloc.buttonLoginA(),
         ],
       ),
       body: Column(
@@ -28,99 +27,5 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
-
-getButtonLogin(){
-    return FutureBuilder(
-            future: getData(),
-            builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                    return Row(
-                        children: <Widget>[
-                            Center(
-                                child: IconButton(
-                                    icon: Icon(Icons.people),
-                                    onPressed: (){navLoginPage(context);},
-                                ),
-                            ),
-                            Center(child: Text("Login")),
-                            Container(height: 10,),
-                        ],
-                    );
-                }
-                if (!snapshot.hasData) {
-                    return Row(
-                        children: <Widget>[
-                            Center(
-                                child: IconButton(
-                                    icon: Icon(Icons.people),
-                                    onPressed: (){navLoginPage(context);},
-                                ),
-                            ),
-                            Center(child: Text("Login")),
-                            Container(height: 10, width: 10,),
-                        ],
-                    );
-                }
-                return getTxt(context);
-            });
-}
-
-
-getTxt(context) {
-  try {
-    return StreamBuilder<List<ModLoginModel>>(
-        stream: modLoginBloc.usersListOut,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return CircularProgressIndicator();
-          }
-          if (!snapshot.hasData) {
-            return CircularProgressIndicator();
-          }
-          final List<ModLoginModel> model = snapshot.data;
-          try {
-            return Row(
-              children: <Widget>[
-                  Icon(Icons.account_box),
-                  Text("Bem vindo ${model[0].username}"),
-                  Center(
-                      child: IconButton(
-                          icon: Icon(Icons.input),
-                          onPressed: (){modLoginBloc.logout();},
-                      ),
-                  ),
-                  Container(height: 10, width: 10,),
-              ],
-            );
-          } catch (e) {
-            return Row(
-                children: <Widget>[
-                    Center(
-                        child: IconButton(
-                            icon: Icon(Icons.people),
-                            onPressed: (){navLoginPage(context);},
-                        ),
-                    ),
-                    Center(child: Text("Login")),
-                    Container(height: 10,),
-                ],
-            );
-          }
-        });
-  } catch (e) {
-    return RaisedButton(
-        color: Theme.of(context).primaryColor,
-        textColor: Colors.white,
-        child: Text("Login"),
-        onPressed: () {
-          navLoginPage(context);
-        });
-  }
-}
-
-getData() async {
-  var login = await model.username;
-  return login;
 }
 
--- lib/app/shared/mod_login/mod_login_bloc.dart
+++ lib/app/shared/mod_login/mod_login_bloc.dart
@@ -1,24 +1,147 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
 import 'package:modulologin/app/shared/mod_login/model/mod_login_model.dart';
+import 'package:modulologin/app/shared/mod_login/utilitario/rotas_login.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ModLoginBloc extends BlocBase {
-
   final ModLoginModel model = ModLoginModel();
 
-  BehaviorSubject<List<ModLoginModel>> _usersListController = BehaviorSubject.seeded([]);
-  Observable<List<ModLoginModel>> get usersListOut => _usersListController.stream;
+  BehaviorSubject<List<ModLoginModel>> _usersListController =
+      BehaviorSubject.seeded([]);
+  Observable<List<ModLoginModel>> get usersListOut =>
+      _usersListController.stream;
 
-  addNovoLogin(ModLoginModel model){
+  addNovoLogin(ModLoginModel model) {
     _usersListController.value.add(model);
     _usersListController.add(_usersListController.value);
   }
 
-  void logout(){
+  void logout() {
     _usersListController.add([]);
     model.token = null;
     model.username = null;
     model.password = null;
+  }
+
+  buttonLoginA() {
+    return FutureBuilder(
+        future: _getDataA(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Row(
+              children: <Widget>[
+                Center(
+                  child: IconButton(
+                    icon: Icon(Icons.people),
+                    onPressed: () {
+                      navLoginPage(context);
+                    },
+                  ),
+                ),
+                Center(child: Text("Login")),
+                Container(
+                  height: 10,
+                ),
+              ],
+            );
+          }
+          if (!snapshot.hasData) {
+            return Row(
+              children: <Widget>[
+                Center(
+                  child: IconButton(
+                    icon: Icon(Icons.people),
+                    onPressed: () {
+                      navLoginPage(context);
+                    },
+                  ),
+                ),
+                Center(child: Text("Login")),
+                Container(
+                  height: 10,
+                  width: 10,
+                ),
+              ],
+            );
+          }
+          return _getOkA(context);
+        });
+  }
+
+  _getOkA(context) {
+    try {
+      return StreamBuilder<List<ModLoginModel>>(
+          stream: usersListOut,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return CircularProgressIndicator();
+            }
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator();
+            }
+            final List<ModLoginModel> model = snapshot.data;
+            try {
+              return Row(
+                children: <Widget>[
+                  Icon(Icons.account_box),
+                  Text("Bem vindo ${model[0].username}"),
+                  Center(
+                    child: IconButton(
+                      icon: Icon(Icons.input),
+                      onPressed: () {
+                        logout();
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    width: 10,
+                  ),
+                ],
+              );
+            } catch (e) {
+              return Row(
+                children: <Widget>[
+                  Center(
+                    child: IconButton(
+                      icon: Icon(Icons.people),
+                      onPressed: () {
+                        navLoginPage(context);
+                      },
+                    ),
+                  ),
+                  Center(child: Text("Login")),
+                  Container(
+                    height: 10,
+                  ),
+                ],
+              );
+            }
+          });
+    } catch (e) {
+      return Row(
+        children: <Widget>[
+          Center(
+            child: IconButton(
+              icon: Icon(Icons.people),
+              onPressed: () {
+                navLoginPage(context);
+              },
+            ),
+          ),
+          Center(child: Text("Login")),
+          Container(
+            height: 10,
+          ),
+        ],
+      );
+    }
+  }
+
+  _getDataA() async {
+    var login = await model.username;
+    return login;
   }
 
   @override
